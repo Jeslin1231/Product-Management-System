8a552039f95271a40dd02d5a967dc617406bf7e0
Machearn <machearn.n@gmail.com>
2024-03-28 19:59:09 -0400

    feat(page): add api request for product list page (#20)

--- src/Pages/ProductList.tsx
+++ src/Pages/ProductList.tsx
@@ -1,31 +1,148 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import logo from '../logo.svg';
-import Button from '../Components/PrimaryButton';
+import { useAppSelector } from '../app/hooks';
+import { selectUser } from '../features/users/UserSlice';
+import { useNavigate } from 'react-router-dom';
+import { get } from '../utils/network';
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+}
+
+interface ProductListState {
+  products: Product[];
+  count: number;
+}
+
+const Order = ['Last Added', 'Price: High to Low', 'Price: Low to High'];
 
 const ProductList = () => {
-  const totalProducts = useRef(200);
   const perPage = useRef(10);
   const maxPrefixPages = useRef(5);
   if (window.innerWidth < 768) {
     perPage.current = 3;
     maxPrefixPages.current = 3;
   }
-  const products = useRef([
-    { id: 1, name: 'Product 1', price: 100 },
-    { id: 1, name: 'Product 1', price: 100 },
-    { id: 1, name: 'Product 1', price: 100 },
-    { id: 1, name: 'Product 1', price: 100 },
-    { id: 1, name: 'Product 1', price: 100 },
-    { id: 1, name: 'Product 1', price: 100 },
-    { id: 1, name: 'Product 1', price: 100 },
-    { id: 1, name: 'Product 1', price: 100 },
-    { id: 1, name: 'Product 1', price: 100 },
-    { id: 1, name: 'Product 1', price: 100 },
-  ]);
-  const pages = Math.ceil(totalProducts.current / perPage.current);
+
+  const navigate = useNavigate();
+  const user = useAppSelector(selectUser);
+  const [productList, setProductList] = useState<ProductListState>({
+    products: [],
+    count: 0,
+  });
   const [currentPage, setCurrentPage] = useState(1);
-  const [sortType, setSortType] = useState('Last Added');
+  const [sortType, setSortType] = useState(0);
   const [detailsOpen, setDetailsOpen] = useState(false);
+  const loading = useMemo(
+    () => (productList.count === 0 ? 'pedding' : 'done'),
+    [productList],
+  );
+
+  const handleAddProduct = () => {
+    navigate('/createProduct');
+  };
+
+  const handleEditProduct = (id: string) => () => {
+    navigate(`/editProduct/${id}`);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let count = 0;
+      if (!user.logged || (user.logged && user.role === 'customer')) {
+        try {
+          const res = await get(
+            'http://localhost:3000/product/get_products_count',
+          );
+          if (res.ok) {
+            const data = await res.json();
+            count = data.count;
+          } else {
+            navigate('/error');
+          }
+        } catch (error) {
+          navigate('/error');
+        }
+
+        try {
+          const res = await get(
+            `http://localhost:3000/product/get_products?page=${currentPage}&limit=${perPage.current}&sort=${sortType}`,
+          );
+          if (res.ok) {
+            const data = await res.json();
+            setProductList({
+              products: data.results,
+              count,
+            });
+          } else {
+            navigate('/error');
+          }
+        } catch (error) {
+          navigate('/error');
+        }
+      } else if (user.logged && user.role === 'vendor') {
+        try {
+          const res = await get(
+            'http://localhost:3000/product/get_vendor_products_count',
+            {
+              headers: {
+                'x-auth-token': user.token,
+              },
+            },
+          );
+          if (res.ok) {
+            const data = await res.json();
+            count = data.count;
+          } else if (res.status === 401) {
+            navigate('/login');
+          } else {
+            navigate('/error');
+          }
+        } catch (error) {
+          navigate('/error');
+        }
+        try {
+          const res = await get(
+            `http://localhost:3000/product/get_vendor_products/?page=${currentPage}&limit=${perPage.current}&sort=${sortType}`,
+            {
+              headers: {
+                'x-auth-token': user.token,
+              },
+            },
+          );
+          if (res.ok) {
+            const data = await res.json();
+            setProductList({
+              products: data.results,
+              count,
+            });
+          } else if (res.status === 401) {
+            navigate('/login');
+          } else {
+            navigate('/error');
+          }
+        } catch (error) {
+          navigate('/error');
+        }
+      }
+    };
+
+    fetchData();
+  }, [user, navigate, currentPage, sortType]);
+
+  if (loading === 'pedding') {
+    return (
+      <div className="flex flex-col items-center justify-center h-full">
+        <p className="text-black text-2xl font-bold">Loading...</p>
+      </div>
+    );
+  }
+
+  const pages = Math.ceil(productList.count / perPage.current);
+
   return (
     <div className="flex flex-col flex-grow w-full items-center">
       <header className="flex flex-col md:flex-row w-11/12 mt-2 md:mt-10">
@@ -42,47 +159,39 @@ const ProductList = () => {
             className="dropdown mr-2 w-1/2"
           >
             <summary className="btn btn-sm md:btn-md w-full">
-              {sortType}
+              {Order[sortType]}
             </summary>
             <ul className="shadow bg-white dropdown-content menu z-[1] w-full">
-              <li>
-                <button
-                  onClick={() => {
-                    setSortType('Last Added');
-                  }}
-                >
-                  Last Added
-                </button>
-              </li>
-              <li>
-                <button
-                  onClick={() => {
-                    setSortType('Price: High to Low');
-                  }}
-                >
-                  Price: High to Low
-                </button>
-              </li>
-              <li>
-                <button
-                  onClick={() => {
-                    setSortType('Price: Low to High');
-                  }}
-                >
-                  Price: Low to High
-                </button>
-              </li>
+              {Order.map((order, index) => (
+                <li key={index}>
+                  <button
+                    onClick={() => {
+                      setSortType(index);
+                    }}
+                  >
+                    {order}
+                  </button>
+                </li>
+              ))}
             </ul>
           </details>
-          <button className="btn btn-primary btn-sm md:btn-md">
-            Add Product
-          </button>
+          {user.logged && user.role === 'vendor' && (
+            <button
+              className="btn btn-primary btn-sm md:btn-md"
+              onClick={handleAddProduct}
+            >
+              Add Product
+            </button>
+          )}
         </div>
       </header>
       <div className="flex flex-col md:grid md:grid-cols-5 flex-grow w-11/12 mt-5 bg-white rounded-md">
-        {products.current.slice(0, perPage.current).map((product, index) => (
-          <div key={index} className="flex flex-col border m-2 border-gray-100">
-            <div className="flex-grow">image</div>
+        {productList.products.map((product, index) => (
+          <div
+            key={index}
+            className="flex flex-col min-h-60 border m-2 border-gray-100"
+          >
+            <img src={product.image} alt={product.name} className="flex-grow" />
             <p className="text-gray-500 text-sm px-1 py-0.5">{product.name}</p>
             <p className="text-black text-xl font-bold px-1 py-0.5">
               ${product.price}
@@ -91,33 +200,60 @@ const ProductList = () => {
               <button className="flex-1 bg-blue-700 rounded text-white text-md mr-1 py-0.5">
                 Add
               </button>
-              <button className="w-5/12 text-black text-md border-gray-200 border py-0.5">
-                Edit
-              </button>
+              {user.logged && user.role === 'vendor' && (
+                <button
+                  className="w-5/12 text-black text-md border-gray-200 border py-0.5"
+                  onClick={handleEditProduct(product.id)}
+                >
+                  Edit
+                </button>
+              )}
             </div>
           </div>
         ))}
       </div>
       <footer className="flex w-11/12 my-2 md:my-5 justify-end">
         <div className="join">
-          <button className="join-item btn btn-sm">«</button>
+          <button
+            className="join-item btn btn-sm"
+            onClick={() => {
+              if (currentPage === 1) return;
+              setCurrentPage(prev => prev - 1);
+            }}
+          >
+            «
+          </button>
           {Array.from({ length: Math.min(pages, maxPrefixPages.current) }).map(
             (_, index) => (
               <button
                 key={index}
                 className={`join-item btn btn-sm ${index + 1 === currentPage && 'btn-primary'}`}
+                onClick={() => setCurrentPage(index + 1)}
               >
                 {index + 1}
               </button>
             ),
           )}
           {pages > 5 && (
-            <button className="join-item btn btn-disabled btn-sm">...</button>
+            <>
+              <button className="join-item btn btn-disabled btn-sm">...</button>
+              <button
+                className="join-item btn btn-sm"
+                onClick={() => setCurrentPage(pages)}
+              >
+                {pages}
+              </button>
+            </>
           )}
-          {pages > 5 && (
-            <button className="join-item btn btn-sm">{pages}</button>
-          )}
-          <button className="join-item btn btn-sm">»</button>
+          <button
+            className="join-item btn btn-sm"
+            onClick={() => {
+              if (currentPage === pages) return;
+              setCurrentPage(prev => prev + 1);
+            }}
+          >
+            »
+          </button>
         </div>
       </footer>
     </div>
